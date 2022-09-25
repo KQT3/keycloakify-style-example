b8cc85b451b7a9eb068fb6b4d258d4e322a7d98f
KQT3 <kristoffer@email.com>
2022-09-25 17:18:11 +0200

    commit

--- src/KcApp/KcApp.tsx
+++ src/KcApp/KcApp.tsx
@@ -4,11 +4,14 @@ import type { KcContext } from "./kcContext";
 import KcAppBase, { defaultKcProps } from "keycloakify";
 import { useI18n } from "./i18n";
 import Login from "./Login";
+import UpdateUserProfile from "./UpdateUserProfile";
+import LoginConfigTotp from "./LoginConfigTotp";
+
 
 const Register = lazy(() => import("./Register"));
 const Terms = lazy(() => import("./Terms"));
-const MyExtraPage1 = lazy(() => import("./MyExtraPage1"));
 const MyExtraPage2 = lazy(() => import("./MyExtraPage2"));
+const MyExtraPage1 = lazy(() => import("./MyExtraPage1"));
 
 export type Props = {
     kcContext: KcContext;
@@ -36,6 +39,8 @@ export default function KcApp({ kcContext }: Props) {
                     case "register.ftl": return <Register {...{ kcContext, ...props }} />;
                     case "login.ftl": return <Login {...{ kcContext, ...props }} />;
                     case "terms.ftl": return <Terms {...{ kcContext, ...props }} />;
+                    case "update-user-profile.ftl": return <UpdateUserProfile {...{ kcContext, ...props }} />;
+                    case "login-config-totp.ftl": return <LoginConfigTotp {...{ kcContext, ...props }} />;
                     case "my-extra-page-1.ftl": return <MyExtraPage1 {...{ kcContext, ...props }} />;
                     case "my-extra-page-2.ftl": return <MyExtraPage2 {...{ kcContext, ...props }} />;
                     default: return <KcAppBase {...{ kcContext, ...props }} />;
---
+++ src/KcApp/UpdateUserProfile.tsx
@@ -0,0 +1,71 @@
+import React, { useState, memo } from "react";
+import Template from "./Template";
+import type { KcProps } from "keycloakify/lib/components/KcProps";
+import type { KcContextBase } from "keycloakify/lib/getKcContext/KcContextBase";
+import { useCssAndCx } from "keycloakify/lib/tools/useCssAndCx";
+import type { I18n } from "keycloakify/lib/i18n";
+import { UserProfileFormFields } from "keycloakify/lib/components/shared/UserProfileCommons";
+
+const UpdateUserProfile = memo(({ kcContext, i18n, ...props }: { kcContext: KcContextBase.UpdateUserProfile; i18n: I18n } & KcProps) => {
+    const { cx } = useCssAndCx();
+
+    const { msg, msgStr } = i18n;
+
+    const { url, isAppInitiatedAction } = kcContext;
+
+    const [isFomSubmittable, setIsFomSubmittable] = useState(false);
+
+    return (
+        <Template
+            {...{ kcContext, i18n, ...props }}
+            doFetchDefaultThemeResources={true}
+            headerNode={msg("loginProfileTitle")}
+            formNode={
+                <form id="kc-update-profile-form" className={cx(props.kcFormClass)} action={url.loginAction} method="post">
+                    <UserProfileFormFields kcContext={kcContext} onIsFormSubmittableValueChange={setIsFomSubmittable} i18n={i18n} {...props} />
+
+                    <div className={cx(props.kcFormGroupClass)}>
+                        <div id="kc-form-options" className={cx(props.kcFormOptionsClass)}>
+                            <div className={cx(props.kcFormOptionsWrapperClass)}></div>
+                        </div>
+
+                        <div id="kc-form-buttons" className={cx(props.kcFormButtonsClass)}>
+                            {isAppInitiatedAction ? (
+                                <>
+                                    <input
+                                        className={cx(props.kcButtonClass, props.kcButtonPrimaryClass, props.kcButtonLargeClass)}
+                                        type="submit"
+                                        value={msgStr("doSubmit")}
+                                    />
+                                    <button
+                                        className={cx(props.kcButtonClass, props.kcButtonDefaultClass, props.kcButtonLargeClass)}
+                                        type="submit"
+                                        name="cancel-aia"
+                                        value="true"
+                                        formNoValidate
+                                    >
+                                        {msg("doCancel")}
+                                    </button>
+                                </>
+                            ) : (
+                                <input
+                                    className={cx(
+                                        props.kcButtonClass,
+                                        props.kcButtonPrimaryClass,
+                                        props.kcButtonBlockClass,
+                                        props.kcButtonLargeClass
+                                    )}
+                                    type="submit"
+                                    defaultValue={msgStr("doSubmit")}
+                                    disabled={!isFomSubmittable}
+                                />
+                            )}
+                        </div>
+                    </div>
+                </form>
+            }
+        />
+    );
+});
+
+export default UpdateUserProfile;
